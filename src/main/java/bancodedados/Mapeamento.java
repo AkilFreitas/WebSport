e4c72712fe446c7f577d02c6154c6e2ebede8ee7
MichaelLucas15 <maycom.lucas10@gmail.com>
2018-06-24 17:29:59 -0300

    Adicionei umas coisas na classe Mapeamento(Pq estava dando erro)

--- src/main/java/bancodedados/Mapeamento.java
+++ src/main/java/bancodedados/Mapeamento.java
@@ -5,33 +5,47 @@
  */
 package bancodedados;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 /**
  *
  * @author Val
  */
-table (name ="Mapeamento") 
+@Entity
+@Table(name = "Mapeamento")
 public class Mapeamento {
-    @id 
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
     private Integer codigo;
-    
+
     @Column(name = "Messagem", nullable = false, length = 1000)
     private String message;
-    
+
     @OneToOne
     @JoinColumn(name = "id")
-    public String getCodigo(){
-     return codigo;;
+    public Integer getCodigo() {
+        return codigo;
     }
-    public void detCodigo(String codigo){
-        this.codigo = codigo; 
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
     }
+
     
-    public String getMessage(){
-     return message;;
+    public String getMessage() {
+        return message;
     }
-    public void setMessage(String Message){
-        this.message = message; 
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
