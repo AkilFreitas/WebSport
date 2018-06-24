e27de8da70f4ca8e06948d905874466033c4dc90
MichaelLucas15 <maycom.lucas10@gmail.com>
2018-06-23 23:36:06 -0300

    Implementação de código na classe FabricaRepositorios

--- src/main/java/infraestrutura/repositorio/implementacoes/fabricaRepositorios/FabricaRepositorios.java
+++ src/main/java/infraestrutura/repositorio/implementacoes/fabricaRepositorios/FabricaRepositorios.java
@@ -5,10 +5,20 @@
  */
 package infraestrutura.repositorio.implementacoes.fabricaRepositorios;
 
+import infraestrutura.repositorio.comportamentos.RepositorioGenerico;
+import infraestrutura.repositorio.implementacoes.RepositorioCliente;
+
 /**
  *
  * @author mayco
  */
 public class FabricaRepositorios {
-    
+
+    //Vai ficar com 1, mas é por enquanto. Quando começar a colocar as outras classes ai ajeita a ordem, no caso CLIENTE, seria o 3, pra ficar na ordem que ta lá no pacote MODEL 
+    public static final int CLIENTE = 1;
+
+    public static final int MEMORIA = 1;
+    public static final int BANCODADOS = 2;
+    public static final int ARQUIVO = 3;
+
 }
