458916fd713bb21738e786c5ede3f7eb6b7994cd
Paulo Neto <paulo-neto2012@outlook.com>
2018-08-08 22:15:22 -0300

    sdfsdf

---
+++ src/main/java/java/model/Endereco.java
@@ -0,0 +1,182 @@
+package java.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+/**
+ * A serialização em Java é o processo no qual a instância de um objeto é
+ * transformada em uma sequência de bytes e é útil quando precisamos enviar
+ * objetos pela rede, salvar no disco, ou comunicar de uma JVM com outra.
+ */
+/**
+ *
+ * @author mayco
+ */
+@Entity
+@Table(name = "endereco")
+public class Endereco implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(length = 20)
+    private String cep;
+    @Column(length = 20)
+    private int numero;
+    @Column(length = 20)
+    private String rua;
+    @Column(length = 20)
+    private String bairro;
+    @Column(length = 20)
+    private String estado;
+    @Column(length = 20)
+    @ManyToMany
+    Cidade cidade;
+
+    public Endereco() {
+    }
+
+    public Endereco(int id, String cep, int numero, String rua, String bairro, String estado, Cidade cidade) {
+        this.id = id;
+        this.cep = cep;
+        this.numero = numero;
+        this.rua = rua;
+        this.bairro = bairro;
+        this.estado = estado;
+        this.cidade = cidade;
+    }
+
+ 
+
+    @Override
+    public String toString() {
+        return "Endereco{" + "id=" + id + ", cep=" + cep
+                + ", numero=" + numero + ", rua=" + rua
+                + ", bairro=" + bairro + ", estado=" + estado
+                + ", cidade=" + cidade + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + this.id;
+        hash = 41 * hash + Objects.hashCode(this.cep);
+        hash = 41 * hash + this.numero;
+        hash = 41 * hash + Objects.hashCode(this.rua);
+        hash = 41 * hash + Objects.hashCode(this.bairro);
+        hash = 41 * hash + Objects.hashCode(this.estado);
+        hash = 41 * hash + Objects.hashCode(this.cidade);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Endereco other = (Endereco) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.numero != other.numero) {
+            return false;
+        }
+        if (!Objects.equals(this.cep, other.cep)) {
+            return false;
+        }
+        if (!Objects.equals(this.rua, other.rua)) {
+            return false;
+        }
+        if (!Objects.equals(this.bairro, other.bairro)) {
+            return false;
+        }
+        if (!Objects.equals(this.estado, other.estado)) {
+            return false;
+        }
+        if (!Objects.equals(this.cidade, other.cidade)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+
+    
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public void manterEndereco() {
+
+    }
+
+    public void setCidade(String garanhuns) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ src/main/java/java/model/Tamanho.java
@@ -0,0 +1,62 @@
+package java.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * A serialização em Java é o processo no qual a instância de um objeto é
+ * transformada em uma sequência de bytes e é útil quando precisamos enviar
+ * objetos pela rede, salvar no disco, ou comunicar de uma JVM com outra.
+ */
+/**
+ * 
+ * @author mayco
+ */
+@Entity
+public class Tamanho implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(length = 20)
+    private String dimensao;
+    @Column(length = 20)
+    private String peso;
+
+    public Tamanho() {
+
+    }
+
+    public Tamanho(int id, String dimensao, String peso) {
+        this.id = id;
+        this.dimensao = dimensao;
+        this.peso = peso;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDimensao() {
+        return dimensao;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDimensao(String dimensao) {
+        this.dimensao = dimensao;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+}
