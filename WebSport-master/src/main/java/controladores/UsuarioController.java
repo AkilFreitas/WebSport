e916e453513e7759707500efd372e4bfdc54ea60
MATEUS MARQUES DE GUSMAO <20151D12GR0227@LABS.GARANHUNS.IFPE>
2018-08-10 13:52:59 -0300

    UsuarioImplBD finalizado

---
+++ WebSport-master/src/main/java/controladores/UsuarioController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import infraestrutura.repositorio.comportamentos.RepositorioGenerico;
+import infraestrutura.repositorio.implementacoes.repositorioImplBD.ClienteImplBD;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.model.Usuario;
+/**
+ *
+ * @author sucesso
+ */
+public class UsuarioController {
+    
+}
---
+++ WebSport-master/src/main/java/infraestrutura/repositorio/implementacoes/repositorioImplBD/PagamentoImplBD.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infraestrutura.repositorio.implementacoes.repositorioImplBD;
+
+/**
+ *
+ * @author 20151D12GR0227
+ */
+public class PagamentoImplBD {
+    
+}
