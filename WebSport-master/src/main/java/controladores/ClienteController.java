1becce6ffb9630d30e7db3886517d3c0b77c01eb
MATEUS MARQUES DE GUSMAO <20151D12GR0227@LABS.GARANHUNS.IFPE>
2018-08-10 14:13:44 -0300

    UsuarioController

--- WebSport-master/src/main/java/controladores/ClienteController.java
+++ WebSport-master/src/main/java/controladores/ClienteController.java
@@ -18,48 +18,47 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import java.model.Cliente;
 
-
 @ManagedBean
 @SessionScoped
 public class ClienteController {
-    
-    private RepositorioGenerico<Cliente,Integer> repositorioCliente = null;
+
+    private RepositorioGenerico<Cliente, Integer> repositorioCliente = null;
     private Cliente clienteCadastro;
     private Cliente selectedCliente;
-   
+
     Cliente cliente = new Cliente();
-    
-    public ClienteController(){
+
+    public ClienteController() {
         this.repositorioCliente = new ClienteImplBD();
         this.clienteCadastro = new Cliente();
     }
-    
-    public void inserir(){
-        
+
+    public void inserir() {
+
         this.repositorioCliente.inserir(this.clienteCadastro);
         this.clienteCadastro = new Cliente();
-        
-        FacesContext.getCurrentInstance().addMessage(null, 
-                new FacesMessage(FacesMessage.SEVERITY_INFO,"Sucesso!","O cliente foi cadastrado com sucesso!"));
+
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "O cliente foi cadastrado com sucesso!"));
     }
-    
-    public void alterar(Cliente c){
+
+    public void alterar(Cliente c) {
         this.repositorioCliente.alterar(c);
-        FacesContext.getCurrentInstance().addMessage(null, 
-                new FacesMessage(FacesMessage.SEVERITY_INFO,"Sucesso!","O cliente foi alterado com sucesso!"));
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "O cliente foi alterado com sucesso!"));
     }
-    
-    public Cliente recuperarCliente(int id){
+
+    public Cliente recuperarCliente(int id) {
         return this.repositorioCliente.recuperar(id);
     }
-    
-    public void deletar(Cliente c){
+
+    public void deletar(Cliente c) {
         this.repositorioCliente.deletar(c);
-        FacesContext.getCurrentInstance().addMessage(null, 
-                new FacesMessage(FacesMessage.SEVERITY_INFO,"Sucesso!","O cliente foi deletado com sucesso!"));
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "O cliente foi deletado com sucesso!"));
     }
-    
-    public List<Cliente> recuperarTodosClientes(){
+
+    public List<Cliente> recuperarTodosClientes() {
         return this.repositorioCliente.recuperarTodos();
     }
 
@@ -79,12 +78,12 @@ public class ClienteController {
         this.selectedCliente = selectedCliente;
     }
 
-    public RepositorioGenerico<Cliente,Integer> getRepositorioCliente() {
+    public RepositorioGenerico<Cliente, Integer> getRepositorioCliente() {
         return repositorioCliente;
     }
 
     public void setRepositorioCliente(RepositorioGenerico<Cliente, Integer> repositorioCliente) {
-        this.repositorioCliente= repositorioCliente;
+        this.repositorioCliente = repositorioCliente;
     }
 
     public Cliente getCliente() {
@@ -94,6 +93,5 @@ public class ClienteController {
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
-    
-    
+
 }
--- WebSport-master/src/main/java/controladores/UsuarioController.java
+++ WebSport-master/src/main/java/controladores/UsuarioController.java
@@ -6,17 +6,92 @@
 package controladores;
 
 import infraestrutura.repositorio.comportamentos.RepositorioGenerico;
-import infraestrutura.repositorio.implementacoes.repositorioImplBD.ClienteImplBD;
+import infraestrutura.repositorio.implementacoes.repositorioImplBD.UsuarioImplBD;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import java.model.Usuario;
+
 /**
  *
  * @author sucesso
  */
+@ManagedBean
+@SessionScoped
 public class UsuarioController {
-    
+
+    private RepositorioGenerico<Usuario, Integer> repositorioUsuario = null;
+    private Usuario usuarioCadastro;
+    private Usuario selectedCliente;
+
+    Usuario usuario = new Usuario();
+
+    public UsuarioController() {
+        this.repositorioUsuario = new UsuarioImplBD();
+        this.usuarioCadastro = new Usuario();
+    }
+
+    public void inserir() {
+
+        this.repositorioUsuario.inserir(this.usuarioCadastro);
+        this.usuarioCadastro = new Usuario();
+
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "O usuario foi cadastrado com sucesso!"));
+    }
+
+    public void alterar(Usuario u) {
+        this.repositorioUsuario.alterar(u);
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "O usuario foi alterado com sucesso!"));
+    }
+
+    public Usuario recuperarUsuario(int id) {
+        return this.repositorioUsuario.recuperar(id);
+    }
+
+    public void deletar(Usuario u) {
+        this.repositorioUsuario.deletar(u);
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", "O usuario foi deletado com sucesso!"));
+    }
+
+    public List<Usuario> recuperarTodosClientes() {
+        return this.repositorioUsuario.recuperarTodos();
+    }
+
+    public RepositorioGenerico<Usuario, Integer> getRepositorioUsuario() {
+        return repositorioUsuario;
+    }
+
+    public void setRepositorioUsuario(RepositorioGenerico<Usuario, Integer> repositorioUsuario) {
+        this.repositorioUsuario = repositorioUsuario;
+    }
+
+    public Usuario getUsuarioCadastro() {
+        return usuarioCadastro;
+    }
+
+    public void setUsuarioCadastro(Usuario usuarioCadastro) {
+        this.usuarioCadastro = usuarioCadastro;
+    }
+
+    public Usuario getSelectedCliente() {
+        return selectedCliente;
+    }
+
+    public void setSelectedCliente(Usuario selectedCliente) {
+        this.selectedCliente = selectedCliente;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
 }
