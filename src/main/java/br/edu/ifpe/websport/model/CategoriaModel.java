47717bc1cd75a54d99046db74e65294c079c7311
MichaelLucas15 <maycom.lucas10@gmail.com>
2018-12-11 11:05:38 -0300

    ..

--- src/main/java/br/edu/ifpe/websport/model/CategoriaModel.java
+++ src/main/java/br/edu/ifpe/websport/model/CategoriaModel.java
@@ -7,7 +7,6 @@ package br.edu.ifpe.websport.model;
 
 import br.edu.ifpe.websport.model.entidades.Categoria;
 import br.edu.ifpe.websport.infraestrutura.repositorio.comportamentos.RepositorioGenerico;
-import br.edu.ifpe.websport.infraestrutura.repositorio.implementacoes.fabricaRepositorios.FabricaRepositorios;
 import br.edu.ifpe.websport.infraestrutura.repositorio.implementacoes.repositorioImplBD.CategoriaImplBD;
 import java.util.List;
 
